0ac8bdf0cfce2d614676caea5b2acbde14e9f832
luisfff29 <luisfff29@gmail.com>
2020-03-19 11:35:56 -0500

    part C done

--- iss.py
+++ iss.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import sys
 import requests
+import turtle
 
 
 __author__ = 'luisfff29 with help from Joseph'
@@ -9,9 +10,12 @@ __author__ = 'luisfff29 with help from Joseph'
 if sys.version_info[0] < 3:
     raise RuntimeError('This program should be run in Python 3')
 
+astronauts_api = 'http://api.open-notify.org/astros.json'
+coordinates_api = 'http://api.open-notify.org/iss-now.json'
+
 
 def part_A():
-    r = requests.get('http://api.open-notify.org/astros.json')
+    r = requests.get(astronauts_api)
     d = r.json()
     print('People in Space: {}'.format(d['number']))
     for p in d['people']:
@@ -19,7 +23,7 @@ def part_A():
 
 
 def part_B():
-    r = requests.get('http://api.open-notify.org/iss-now.json')
+    r = requests.get(coordinates_api)
     d = r.json()
     print('Geographic location:')
     for l, num in d['iss_position'].items():
@@ -27,11 +31,33 @@ def part_B():
     print('Timestamp: {}'.format(d['timestamp']))
 
 
+def part_C():
+    s = turtle.Screen()
+    t = turtle.Turtle()
+
+    s.setup(width=720, height=360)
+    s.bgpic('map.gif')
+    s.title('ISS Location')
+    s.addshape('iss.gif')
+    s.setworldcoordinates(-180, -90, 180, 90)
+
+    t.penup()
+    t.shape('iss.gif')
+    while True:
+        r = requests.get(coordinates_api).json()
+        lon, lat = r['iss_position']['longitude'], r['iss_position']['latitude']
+        t.goto(float(lon), float(lat))
+
+    turtle.done()
+
+
 def main(args):
     if args[0] == 'part_A':
         part_A()
     elif args[0] == 'part_B':
         part_B()
+    elif args[0] == 'part_C':
+        part_C()
 
 
 if __name__ == '__main__':
